648691ba81b08084d7ee7d7e8106ab3b7c2d3185
Kiên <86771355+toilakien@users.noreply.github.com>
2022-08-22 16:38:38 +0700

    update

--- src/views/OverView/index.tsx
+++ src/views/OverView/index.tsx
@@ -1,8 +1,34 @@
-const OverView=()=>{
-    return (
-        <div>
-            Over VIew
+import { Container, Grid, styled, Typography } from '@mui/material';
+import PageTitleWrapper from 'src/components/PageTitleWrapper';
+import ReportGmailerrorredIcon from '@mui/icons-material/ReportGmailerrorred';
+import '../../styles/overview.css';
+import BoxItem from './BoxItem';
+const OverView = () => {
+  const OverViewWrapper = styled(Grid)({
+    p: 2,
+    borderRadius: '10px',
+    backgroundColor: '#84ffff',
+    height: '100vh',
+    width: '100%'
+  });
+
+  return (
+    <Container>
+      <PageTitleWrapper>
+        <Typography>Over view</Typography>
+      </PageTitleWrapper>
+      <OverViewWrapper>
+        <Typography variant='h2' sx={{ color:"#303f9f",p:2,textAlign: 'center' }}>
+          STATISTICS OF RECRUITMENT RESULTS
+        </Typography>
+        <div className="row">
+          <BoxItem clBox='bgRed'clBtn='bgRedBtn' number={'42'} question="Question"/> 
+          <BoxItem clBox='bgYellow'clBtn='bgYellowBtn' number={'6'} question="Department"/> 
+          <BoxItem clBox='bgBlue'clBtn='bgBlueBtn' number={'46'} question="Rank"/> 
+          <BoxItem clBox='bgGreen'clBtn='bgGreenBtn' number={'23'} question="Language"/> 
         </div>
-    )
-}
+      </OverViewWrapper>
+    </Container>
+  );
+};
 export default OverView;
