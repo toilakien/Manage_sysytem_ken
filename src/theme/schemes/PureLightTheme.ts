6933d650830b5d13b9eff840369861bd13d9bb6e
toilakien <kienle17042001@gmail.com>
2022-11-03 11:50:14 +0700

    fix bug

--- src/theme/schemes/PureLightTheme.ts
+++ src/theme/schemes/PureLightTheme.ts
@@ -323,19 +323,19 @@ export const PureLightTheme = createTheme({
     }
   },
   components: {
-    MuiBackdrop: {
-      styleOverrides: {
-        root: {
-          backgroundColor: alpha(darken(themeColors.primaryAlt, 0.4), 0.2),
-          backdropFilter: 'blur(2px)',
+    // MuiBackdrop: {
+    //   styleOverrides: {
+    //     root: {
+    //       backgroundColor: alpha(darken(themeColors.primaryAlt, 0.4), 0.2),
+    //       backdropFilter: 'blur(2px)',
 
-          '&.MuiBackdrop-invisible': {
-            backgroundColor: 'transparent',
-            backdropFilter: 'blur(2px)'
-          }
-        }
-      }
-    },
+    //       '&.MuiBackdrop-invisible': {
+    //         backgroundColor: 'transparent',
+    //         backdropFilter: 'blur(2px)'
+    //       }
+    //     }
+    //   }
+    // },
     MuiFormHelperText: {
       styleOverrides: {
         root: {
