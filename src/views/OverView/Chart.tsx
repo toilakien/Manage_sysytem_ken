27dd922cea06023fdac2c32ad8f57b19a2faee32
toilakien <kienle17042001@gmail.com>
2022-11-09 14:37:22 +0700

    update

--- src/views/OverView/Chart.tsx
+++ src/views/OverView/Chart.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useTheme } from '@mui/material/styles';
-import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
 import Title from './Title';
+import { Label, Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
 
 // Generate Sales Data
 function createData(time: string, amount?: number) {
